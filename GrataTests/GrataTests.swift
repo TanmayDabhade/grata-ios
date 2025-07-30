0ba55aa2b88bfc7102400687b390ae3c02652c81
Tanmay Dabhade <tanmaydabhade129@gmail.com>
2025-07-30 10:40:36 +0530

    Initial Commit

---
+++ GrataTests/GrataTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrataTests.swift
+//  GrataTests
+//
+//  Created by Tanmay Avinash Dabhade on 7/30/25.
+//
+
+import Testing
+@testable import Grata
+
+struct GrataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
